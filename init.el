0a62fdfac855c252a6845535a641ef41ac9e4d73
Hao Wei <imwillmouse@gmail.com>
2015-01-03 14:22:16 +0800

    updated

--- init.el
+++ init.el
@@ -1,37 +1,49 @@
+;; Path
 (push "/usr/local/bin" exec-path)
 
+;; Package
 (require 'package)
 (add-to-list 'package-archives
              '("melpa" . "http://melpa.org/packages/") t)
 (package-initialize)
 
 (defvar my-packages '(better-defaults
-		      projectile
+                      projectile
                       clojure-mode
                       cider
                       evil
                       evil-leader
                       company
                       neotree
-		      flx-ido
-		      ido-vertical-mode
+                      flx-ido
+                      ido-vertical-mode
                       ido-ubiquitous
                       ag
                       smex
                       guide-key
-                      hideshowvis
-		      ))
+                      hideshowvis))
 
 (dolist (p my-packages)
   (unless (package-installed-p p)
     (package-install p)))
 
+;; Common
 (fset 'yes-or-no-p 'y-or-n-p)
-
 (setq make-backup-files nil)
 (setq auto-save-default nil)
+(setq-default tab-width 2)
+(setq-default indent-tabs-mode nil)
 (setq inhibit-startup-message t)
+(global-linum-mode 1)
 
+;; Font
+(set-face-attribute 'default nil :font "Monaco-14")
+
+;; Theme
+(add-to-list 'custom-theme-load-path "~/.emacs.d/themes")
+(load-theme 'dichromacy t)
+
+;; Mode Line
 (setq-default mode-line-format
   (list
      (propertize "%I " 'face 'font-lock-constant-face) ;; size
@@ -43,7 +55,7 @@
     ;; line and column
     "(" ;; '%02' to set to 2 chars at least; prevents flickering
       (propertize "%l" 'face 'font-lock-type-face) ","
-      (propertize "%c" 'face 'font-lock-type-face) 
+      (propertize "%c" 'face 'font-lock-type-face)
     ") "
 
     ;; the current major mode for the buffer.
@@ -60,6 +72,7 @@
     ;; '(:eval minor-mode-alist)
     ))
 
+;; Evil
 (setq-default evil-want-C-u-scroll t)
 (global-evil-leader-mode)
 (evil-leader/set-key "u" 'universal-argument)
@@ -67,35 +80,6 @@
 (evil-mode 1)
 (evil-leader/set-leader ",")
 
-(smex-initialize)
-(global-set-key (kbd "M-x") 'smex)
-(global-set-key (kbd "M-X") 'smex-major-mode-commands)
-;; This is your old M-x.
-(global-set-key (kbd "C-c C-c M-x") 'execute-extended-command)
-
-(add-hook 'clojure-mode-hook 'hideshowvis-enable)
-(add-hook 'emacs-lisp-mode-hook 'hideshowvis-enable)
-(add-hook 'ruby-mode-hook 'hideshowvis-enable)
-(add-hook 'python-mode-hook 'hideshowvis-enable)
-(hideshowvis-symbols)
-(define-key evil-normal-state-map "zA" 'hs-hide-all)
-(define-key evil-normal-state-map "zB" 'hs-show-all)
-
-(global-linum-mode 1)
-
-(projectile-global-mode)
-(define-key evil-normal-state-map "\C-p" 'projectile-find-file)
-
-(setq guide-key/guide-key-sequence t)
-(guide-key-mode 1)
-
-(defun toggle-comment-on-line ()
-  "comment or uncomment current line"
-  (interactive)
-  (comment-or-uncomment-region (line-beginning-position) (line-end-position)))
-(define-key evil-normal-state-map "gc" 'toggle-comment-on-line)
-
-(define-key evil-insert-state-map "j" #'cofi/maybe-exit)
 (evil-define-command cofi/maybe-exit ()
   :repeat change
   (interactive)
@@ -111,31 +95,63 @@
     (push 'escape unread-command-events))
        (t (setq unread-command-events (append unread-command-events
                           (list evt))))))))
+(define-key evil-insert-state-map "j" #'cofi/maybe-exit)
 
+;; Smex
+(smex-initialize)
+(global-set-key (kbd "M-x") 'smex)
+(global-set-key (kbd "M-X") 'smex-major-mode-commands)
+;; This is your old M-x.
+(global-set-key (kbd "C-c C-c M-x") 'execute-extended-command)
+
+;; Hideshowvis
+(add-hook 'clojure-mode-hook 'hideshowvis-enable)
+(add-hook 'emacs-lisp-mode-hook 'hideshowvis-enable)
+(add-hook 'ruby-mode-hook 'hideshowvis-enable)
+(add-hook 'python-mode-hook 'hideshowvis-enable)
+(hideshowvis-symbols)
+(define-key evil-normal-state-map "zA" 'hs-hide-all)
+(define-key evil-normal-state-map "zB" 'hs-show-all)
+
+;; Highlight Chars
+(require 'highlight-chars)
+(add-hook 'font-lock-mode-hook 'hc-highlight-tabs)
+(add-hook 'font-lock-mode-hook 'hc-highlight-trailing-whitespace)
+
+;; Projectile
+(projectile-global-mode)
+(define-key evil-normal-state-map "\C-p" 'projectile-find-file)
+
+;; Guide Key
+(setq guide-key/guide-key-sequence t)
+(guide-key-mode 1)
+
+;; Comment toggling
+(defun toggle-comment-on-line ()
+  "comment or uncomment current line"
+  (interactive)
+  (comment-or-uncomment-region (line-beginning-position) (line-end-position)))
+(define-key evil-normal-state-map "gc" 'toggle-comment-on-line)
+
+;; Ido
 (ido-mode 1)
 (ido-vertical-mode 1)
 (ido-everywhere 1)
 (flx-ido-mode 1)
-;; disable ido faces to see flx highlights.
 (setq ido-enable-flex-matching t)
 (setq ido-use-faces nil)
-
 (ido-ubiquitous-mode 1)
 
-(set-face-attribute 'default nil :font "Monaco-14")
-
+;; Neotree
 (setq neo-window-width 32
       neo-banner-message nil
       neo-auto-indent-point t
       neo-persist-show nil
       neo-dont-be-alone t)
-
 (evil-leader/set-key "l" 'neotree-toggle)
 (add-hook 'neotree-mode-hook
           (lambda ()
             (define-key evil-normal-state-local-map (kbd "o") 'neotree-enter)))
 
-(add-to-list 'custom-theme-load-path "~/.emacs.d/themes")
-(load-theme 'dichromacy t)
-
+;; Start server
 (server-start)
