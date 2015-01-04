624ebc50950769b33efa9a18c9e41ece3eb3528f
Hao Wei <imwillmouse@gmail.com>
2015-01-04 21:16:00 +0800

    lua-mode

--- init.el
+++ init.el
@@ -14,6 +14,7 @@
                       web-mode
                       js2-mode
                       scala-mode2
+                      lua-mode
                       yaml-mode
                       markdown-mode
                       scss-mode
@@ -154,6 +155,7 @@
 (add-hook 'emacs-lisp-mode-hook 'hideshowvis-enable)
 (add-hook 'ruby-mode-hook 'hideshowvis-enable)
 (add-hook 'python-mode-hook 'hideshowvis-enable)
+(add-hook 'lua-mode-hook 'hideshowvis-enable)
 (add-hook 'web-mode-hook 'hideshowvis-enable)
 (add-hook 'js2-mode-hook 'hideshowvis-enable)
 (add-hook 'coffee-mode-hook 'hideshowvis-enable)
