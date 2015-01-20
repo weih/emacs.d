e7adf75a16c099f11336ae8a8f4c5611cac45780
Hao Wei <imwillmouse@gmail.com>
2015-01-20 16:23:02 +0800

    add intent guide & os x color picker.

--- init.el
+++ init.el
@@ -41,6 +41,7 @@
                       ctags
                       expand-region
                       multiple-cursors
+                      indent-guide
                       git-timemachine
                       git-gutter+
                       jazz-theme
@@ -198,6 +199,9 @@
 (evil-leader/set-key "mp" 'magit-push)
 (evil-leader/set-key "md" 'magit-diff-unstaged)
 
+;; Indent Guide
+(indent-guide-global-mode)
+
 ;; Anzu
 (global-anzu-mode 1)
 (defun my-anzu-update-func (here total)
@@ -315,6 +319,50 @@
             (define-key evil-normal-state-local-map (kbd "o") 'neotree-enter)
             (define-key evil-normal-state-local-map (kbd "q") 'neotree-hide)
             (define-key evil-normal-state-local-map (kbd "r") 'neotree-refresh)))
+
+;; Use OS X Color Picker in Emacs
+(defun custom-color--choose-action (widget &optional _event) ; this function is only needed if you want to use color-picker in Easy Customization
+  "customize `widget-color--chose-action' to not split the screen"
+  (list-colors-display
+   nil nil
+   `(lambda (color)
+      (when (buffer-live-p ,(current-buffer))
+  (widget-value-set ',(widget-get widget :parent) color)
+        (pop-to-buffer ,(current-buffer))))))
+
+(defun nscolor2hex (color)
+  "Converts colors from `NSColor' format to hex values"
+  (concat "#"                           ; Add # at the front
+          (mapconcat 'identity          ; concate the list
+                     (mapcar '(lambda (x) ;returns ("hex" "hex" "hex")
+                                (let ((col (lsh (string-to-int x) -8)))
+                                  (if (< col 16)
+                                      (format "0%x" col)
+                                    (format "%x" col))))
+                             (split-string (s-replace "\"" "" color) ",")) "")))
+
+(defun color-picker (&optional list buffer-name callback)
+  "Calls OS X color picker and insert the chosen color. It is really messy because of applyscript"
+  (interactive)
+  (let ((result
+         (do-applescript "tell application \"Finder\"
+  activate
+set result to \"\"
+set x to (choose color)
+set result to item 1 of x as string
+set result to result & \",\"
+set result to result & item 2 of x as string
+set result to result & \",\"
+set result to result & item 3 of x as string
+return result
+end tell")))
+    (if callback ; For Easy Customization
+        (funcall callback (nscolor2hex result))
+      (insert (nscolor2hex result)))
+    (do-applescript "tell application \"Emacs\" to activate")))
+
+(defalias 'list-colors-display 'color-picker)
+(defalias 'widget-color--choose-action 'custom-color--choose-action)
 
 ;; Start server
 (server-start)
